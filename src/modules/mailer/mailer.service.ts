919b4159f01e559afc96a63370d75102f1640d29
ranamiah221 <ranarasul21@gmail.com>
2025-09-18 00:02:38 +0600

    added try catch into the mailer

--- src/modules/mailer/mailer.service.ts
+++ src/modules/mailer/mailer.service.ts
@@ -16,20 +16,28 @@ export class MailerService {
   }
 
   async sendVerificationEmail(email: string, code: string) {
-    await this.transporter.sendMail({
-      from: '"MyApp" <no-reply@myapp.com>',
-      to: email,
-      subject: 'Email Verification Code',
-      html: `<p>Your verification code: <b>${code}</b></p>`,
-    });
+    try {
+      await this.transporter.sendMail({
+        from: '"MyApp" <no-reply@myapp.com>',
+        to: email,
+        subject: 'Email Verification Code',
+        html: `<p>Your verification code: <b>${code}</b></p>`,
+      });
+    } catch (err) {
+      console.log(err)
+    }
   }
 
   async sendPasswordResetEmail(email: string, code: string) {
-    await this.transporter.sendMail({
-      from: '"MyApp" <no-reply@myapp.com>',
-      to: email,
-      subject: 'Password Reset Code',
-      html: `<p>Your password reset code: <b>${code}</b></p>`,
-    });
+    try {
+      await this.transporter.sendMail({
+        from: '"MyApp" <no-reply@myapp.com>',
+        to: email,
+        subject: 'Password Reset Code',
+        html: `<p>Your password reset code: <b>${code}</b></p>`,
+      });
+    } catch (err) {
+      console.log(err)
+    }
   }
 }
