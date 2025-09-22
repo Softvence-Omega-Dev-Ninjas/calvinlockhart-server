9c66a9a84143b888fc48ce44e95d114e02be4e2c
ranamiah221 <ranarasul21@gmail.com>
2025-09-23 04:58:02 +0600

    precepts off the day

--- src/modules/bibles/precept/precept.controller.ts
+++ src/modules/bibles/precept/precept.controller.ts
@@ -5,24 +5,55 @@ import axios from "axios";
 
 @Controller("random-content")
 export class RandomContentController {
-  private readonly baseUrl = "https://bible.helloao.org/api/eng_kjv/books.json";
+ private readonly baseUrl =
+    "https://bible.helloao.org/api/eng_kjv/books.json";
 
-  constructor(private readonly randomContentService: RandomContentService) {}
+  // Cache storage
+  private cachedVerse: {
+    bookName: string;
+    chapterNumber: number;
+    verseNumber: number;
+    content: string;
+    timestamp: number;
+  } | null = null;
 
   @Get()
   async getRandomVerse() {
+    const now = Date.now();
+    const ONE_DAY = 24 * 60 * 60 * 1000; 
+
+    if(this.cachedVerse && now - this.cachedVerse.timestamp< ONE_DAY ){
+      return this.cachedVerse;
+    }
+
     try {
       const response = await axios.get(`${this.baseUrl}`);
       const books = response.data.books;
+   
       const random = Math.floor(Math.random() * books.length) + 1;
+      
+      const seletedBook = books[random];
+      const UniqueId = seletedBook.id;
 
       const uniqueId = books[random].id;
       const chapterRes = await axios.get(
         `https://bible.helloao.org/api/eng_kjv/${uniqueId}/1.json`,
       );
-
       const chapContent = chapterRes.data.chapter.content;
-      return chapContent[0].content[0];
+      
+      const firstVerse = chapContent[0]?.content[0] || "";
+      const verseNumber = chapContent[0].content[0]?.number || 1;
+      const chapterNumber = chapContent.number || 1;
+    
+      this.cachedVerse = {
+        bookName:seletedBook.name,
+        chapterNumber,
+        verseNumber,
+        content: firstVerse,
+        timestamp:now
+      };
+
+      return this.cachedVerse;
     } catch (err) {
       console.error(err);
       throw new BadRequestException("Failed to get random verse");
